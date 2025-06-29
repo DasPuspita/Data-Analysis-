1ed35c4b2badd65c5e5715b2d39968ad30e62162
Puspita Das <142655415+DasPuspita@users.noreply.github.com>
2025-06-29 19:37:43 +0600

    Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -6,35 +6,35 @@
 
 -- Create another table to work on this table instead of raw data
 
-create table layoffs_stage3
+create table layoffs_staging
 like layoffs;
 
-select * from layoffs_stage3;
+select * from layoffs_staging;
 
-insert layoffs_stage3
+insert layoffs_staging
 select*from layoffs;
 
-select * from layoffs_stage3;
+select * from layoffs_staging;
 
 -- Remove duplicates
 
 select *,
 row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) as row_num 
-from layoffs_stage3
+from layoffs_staging
 having row_num>1;
 
 -- CREATE CTE TO COUNT DUPLICATES THROUGH ROW_NUMBER
 with duplicate_check as
 (select *,
 row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) as row_num 
-from layoffs_stage3)
+from layoffs_staging)
 
 select*from duplicate_check
 where row_num>1;
 
 -- CREATE ANOTHER TABLE TO UPDATE OR DELETE 
 
-CREATE TABLE `layoffs_stage4` (
+CREATE TABLE `layoffs_staging2` (
   `company` text,
   `location` text,
   `industry` text,
@@ -48,78 +48,78 @@ CREATE TABLE `layoffs_stage4` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- inserting data with row number as well
-select*from layoffs_stage4;
-insert into layoffs_stage4
+select*from layoffs_staging2;
+insert into layoffs_staging2
 select *,
 row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) as row_num 
-from layoffs_stage3;
+from layoffs_staging;
 
-select * from layoffs_stage4
+select * from layoffs_staging2
 where row_num>1;
 
 -- deleting column with same values
 delete 
-from layoffs_stage4
+from layoffs_staging2
 where row_num>1;
 
-select * from layoffs_stage4
+select * from layoffs_staging2
 where row_num>1;
 
-select * from layoffs_stage4;
+select * from layoffs_staging2;
 
 -- standarized data
 -- removing white space
 select company, trim(company)
-from layoffs_stage4;
+from layoffs_staging2;
 
 -- updating with values without white space
-update layoffs_stage4
+update layoffs_staging2
 set company= trim(company);
 
-select distinct company from layoffs_stage4;
+select distinct company from layoffs_staging2;
 
-select distinct industry from layoffs_stage4;
+select distinct industry from layoffs_staging2;
 
 select distinct industry
-from layoffs_stage4
+from layoffs_staging2
 where industry like 'Crypto%';
 
 -- updating values with same type of thing
-update layoffs_stage4
+update layoffs_staging2
 set industry= 'Crypto'
 where industry like 'Crypto%';
 
 select distinct country 
-from layoffs_stage4; 
+from layoffs_staging2; 
 
 select distinct country, trim(trailing '.' from country)
-from layoffs_stage4;
+from layoffs_staging2;
 
 -- updating without mark in the same name to find unique value
-update layoffs_stage4
+update layoffs_staging2
 set country= trim(trailing '.' from country); 
 
 select distinct country
-from layoffs_stage4; 
+from layoffs_staging2; 
 
 select `date`, 
 str_to_date(`date`, '%m/%d/%Y')
-from layoffs_stage4 
+from layoffs_staging2 
 ;
 
 -- updating date
-update layoffs_stage4
+update layoffs_staging2
 set `date` = str_to_date(`date`, '%m/%d/%Y'); 
 
 select *
-from layoffs_stage4;  
+from layoffs_staging2;  
 
 -- change type of date
-alter table layoffs_stage4
+alter table layoffs_staging2
 modify column `date` DATE; 
 
 select *
-from layoffs_stage4;   
+from layoffs_staging2;   
 
 -- Populate Null/Blank values
 
